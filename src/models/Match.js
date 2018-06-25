e7649ad11fcf30039e08a680a565c52572c51500
Andre Azzolini <apazzolini@gmail.com>
2018-06-25 00:18:09 -0500

    Ensure sample match is a valid one

--- src/models/Match.js
+++ src/models/Match.js
@@ -117,6 +117,7 @@ const Match = {
             FROM match_players mp
             JOIN matches m ON mp.match_id = m.id
             WHERE m.shard_id = ${shardId}
+            AND played_at IS NOT NULL
             ORDER BY m.played_at DESC
             LIMIT 1
         `, { debug })
